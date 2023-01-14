da5e0929a6f6ff23f2b351da93e8a9a33a852093
Andrej Vujić <vujicandrej365@gmail.com>
2023-01-14 18:16:39 +0100

    remove unused methods

--- src/camera.py
+++ src/camera.py
@@ -29,6 +29,9 @@ class VideoCamera:
         time.sleep(2.0)
 
     def kill(self) -> None:
+        print(
+            "[RPiCamera] Releasing...",
+        )
         self.cap.release()
 
     def apply_flips(self, frame: Any) -> Any:
@@ -42,36 +45,18 @@ class VideoCamera:
 
     def get_frame(self) -> Any:
         _, frame = self.cap.read()
-        frame = self.apply_flips(
-            frame=frame,
-        )
-        return frame
 
         if _:
-            frame = self.zoom(frame=frame)
+            frame = self.apply_flips(
+                frame=frame,
+            )
 
-            self.previous_frame = frame
             return frame
 
         return self.previous_frame
 
-    def get_image_bytes(self) -> Any:
-        frame = self.get_frame()
-        _, image = cv2.imencode(
-            ".jpg",
-            frame,
-        )
-        return image.tobytes()
-
     def get_image(self) -> Any:
-        frame = self.get_frame()
-        return frame
-
-        _, image = cv2.imencode(
-            ".jpg",
-            frame,
-        )
-        return image
+        return self.get_frame()
 
     def save_image(self) -> int:
         image = self.get_image()
@@ -86,46 +71,3 @@ class VideoCamera:
         )
 
         return id
-
-    def zoom(self, frame: Any, coord=None) -> Any:
-        h, w, _ = [self.zoom_factor * i for i in frame.shape]
-
-        if coord is None:
-            cx, cy = w / 2, h / 2
-        else:
-            cx, cy = [self.zoom_factor * c for c in coord]
-
-        frame = cv2.resize(
-            frame,
-            (0, 0),
-            fx=self.zoom_factor, fy=self.zoom_factor,
-        )
-
-        frame = frame[
-            int(
-                round(
-                    cy - h / self.zoom_factor * 0.5,
-                ),
-            ): int(
-                round(
-                    cy + h / self.zoom_factor * 0.5,
-                ),
-            ),
-            int(
-                round(
-                    cx - w / self.zoom_factor * 0.5,
-                ),
-            ): int(
-                round(
-                    cx + w / self.zoom_factor * 0.5,
-                ),
-            ),
-            :]
-
-        return frame
-
-    def to_grayscale(self, frame: Any) -> Any:
-        return cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-
-    def to_color(self, frame: Any) -> Any:
-        return cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
